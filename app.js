df8d80125c02853d1bf12e7354f6d96e85d89138
Ciane NYAMBO <cianekaj@gmail.com>
2021-07-14 04:48:55 -0600

    changes made on ondex.html, add css and js files

---
+++ app.js
@@ -0,0 +1,17 @@
+function randColor()
+{
+     const color1 = Math.floor(Math.random()*256)
+     const color2 = Math.floor(Math.random()*256)
+     const color3 = Math.floor(Math.random()*256)
+     return `rgb(${color1},${color2},${color3})`;
+}
+const letters = document.querySelectorAll('.letter')
+setInterval(function(){
+    for (let letter of letters)
+    {
+        letter.style.color = randColor()
+    }
+    
+}, 1000);
+const h = document.querySelector('h1');
+h.style.textAlign="center";
